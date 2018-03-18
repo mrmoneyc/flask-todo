3b784180efbb561d364453a037c4ae0d5d94889e
mrmoneyc <moneyc.net@gmail.com>
2018-03-18 19:16:08 +0800

    feat: todo api CRUD mock

--- src/app.py
+++ src/app.py
@@ -21,5 +21,30 @@ class HelloResource(Resource):
         return resp_body
 
 
+@api.route('/todo')
+@api.route('/todo/<int:todo_id>')
+class TodoResource(Resource):
+    def get(self, todo_id=None):
+        if not todo_id:
+            return 'Get all todos'
+        else:
+            return 'Get todo by id: %d' % todo_id
+
+    def post(self):
+        return 'Create todo'
+
+    def put(self, todo_id=None):
+        if not todo_id:
+            return 'Todo ID is required'
+        else:
+            return 'Update todo by id: %d' % todo_id
+
+    def delete(self, todo_id=None):
+        if not todo_id:
+            return 'Todo ID is required'
+        else:
+            return 'Delete todo by id: %d' % todo_id
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="0.0.0.0")
