ee2cabc17f2c06e84db83c911596c8aae29f7b78
mrmoneyc <moneyc.net@gmail.com>
2018-03-19 13:54:37 +0800

    fix: accept utf-8 character

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql import expression
 from marshmallow import Schema, fields, ValidationError
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://todo:todo@127.0.0.1:3306/todo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://todo:todo@127.0.0.1:3306/todo?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 api = Api(app)
